099b244e6c22b270e15bc8601e994756dddaabfc
Adnan Hashmi <hashmi.adnan791@gmail.com>
2021-10-19 18:24:41 +0530

    init

---
+++ components/CTA.js
@@ -0,0 +1,18 @@
+// import './cta.css';
+
+function CTA() {
+  return (
+    <>
+      <div className="waves">
+        <></>
+      </div>
+      <style jsx>{`
+        .waves {
+          height: 50rem;
+        }
+      `}</style>
+    </>
+  );
+}
+
+export default CTA;
